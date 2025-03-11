e76c06ac640d527a1e75cb989bf0eeed3da41170
LuckyChaudhari05 <166679369+LuckyChaudhari05@users.noreply.github.com>
2025-03-11 13:36:18 +0530

    abstractionequailty.js and coercion.js are created and modified

---
+++ Javascript/1st Pillar - Coercion/abstractionequailty.js
@@ -0,0 +1,34 @@
+// console.log(null == undefined);
+// console.log(null == NaN);
+// console.log(NaN == undefined);
+// // console.log(null == 0);
+// console.log(true == "1");
+// console.log(false == "1");
+// console.log(false == "0");
+
+// console.log(Number(true));  // Manual type conversion 
+// console.log(Number(false));
+
+// console.log("1" === NaN);   // 
+
+console.log(typeof(null));
+console.log(NaN === NaN);
+console.log("the new way of the world is");
+
+console.log(100 === 99);
+
+let a = "abc";
+let b = "abc";
+console.log(a === b);
+console.log(false === false);
+
+let x = {
+  name: "lucky",
+  age: 19
+}
+let y = {
+  name: "lucky",
+  age: 19
+}
+console.log(x === y);
+console.log(x === x);
---
+++ Javascript/1st Pillar - Coercion/coercion.js
@@ -0,0 +1,4 @@
+let num = 10;
+let obj = {x:12,y:10};
+console.log(`The number is the ${num}`);  // the num is converted in the string 
+console.log(`The number is the ${obj}`);  
