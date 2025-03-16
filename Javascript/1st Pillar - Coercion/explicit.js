6c1e5587336d747543840c1602ab30b71985ec69
LuckyChaudhari05 <166679369+LuckyChaudhari05@users.noreply.github.com>
2025-03-16 15:15:42 +0530

    explicit.js was created and modified

---
+++ Javascript/1st Pillar - Coercion/explicit.js
@@ -0,0 +1,14 @@
+// console.log(String(NaN));
+
+
+
+function signs(x) {
+  if( Math.sign(x) == 1 || 0 ) {
+    console.log("Sing is ' + ' ");
+  }
+  else {
+    console.log("Sing is ' - '");
+  }
+}
+signs(-10);
+console.log(Math.sign(-10));
