4ae52a7b493945935a2fb55bdda922c0f684fab0
LuckyChaudhari05 <166679369+LuckyChaudhari05@users.noreply.github.com>
2025-06-29 16:39:09 +0530

    Interview Question done on SetTimeout

---
+++ Javascript/4th Pillar - Callback's/question/2.js
@@ -0,0 +1,8 @@
+console.log("Start");
+setTimeout(() => {
+  console.log("setTime");
+}, 0);
+for(let i = 0 ; i<= 10000000000;i++){
+    // some task
+  }
+console.log("End");
---
+++ Javascript/4th Pillar - Callback's/question/3.js
@@ -0,0 +1,12 @@
+console.log("Start");
+
+for(let i = 1; i<4;i++){
+setTimeout(() => {
+  console.log(`setTime ${i}`);
+}, 500-(i*2));
+}
+
+for(let i = 0 ; i<= 10000000000;i++){
+    // some task
+  }
+console.log("End");
