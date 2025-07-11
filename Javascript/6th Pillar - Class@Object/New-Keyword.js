4344b5a181c2a6381665df6f37610ba35cd1a91f
LuckyChaudhari05 <166679369+LuckyChaudhari05@users.noreply.github.com>
2025-07-11 19:53:10 +0530

    Add product array and demo for category retrieval

---
+++ Javascript/6th Pillar - Class@Object/New-Keyword.js
@@ -0,0 +1,10 @@
+let Iphone = {
+  name : "Iphone 13",
+  price : 100000,
+  rating : 4.5,
+  dis : "This is the new iphone 13",
+
+  display () {
+    console.log("First Product : ",this);
+  }
+}
---
+++ Javascript/6th Pillar - Class@Object/Object-Demo2.js
@@ -0,0 +1,21 @@
+let products  = {
+  arr : [
+    {
+      name : "Iphone 13",
+      price : 100000,
+      rating : 4.5,
+      dis : "This is the new iphone 13",
+      category : "Eletronic Product's"
+    },
+    {
+       name : "Onepluse Node 2",
+        price : 50000,
+        rating : 4.9,
+        dis : "This is the new Onepluse Node 2",
+        category : "Eletronic Product's"
+    }
+  ],
+  
+}
+// products.arr[0].getCategory()
+// console.log(products.arr[0].getCategory());
